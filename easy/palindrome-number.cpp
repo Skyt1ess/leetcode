c42bfc5ba28c37fd948c8bee85b15c64c1b8f047
Islam <35372062+Skyt1ess@users.noreply.github.com>
2023-03-08 00:09:32 +0300

    Update palindrome-number.cpp

--- easy/palindrome-number.cpp
+++ easy/palindrome-number.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long y = x % 10, z = x / 10;
-        while (z) {
-            y = y * 10 + z % 10;
-            z /= 10;
+        if (x < 0) {
+            return false;
         }
-        return (x >= 0 && x == y);
+        int k = 0, y = 0;
+        for (int z = x; z; z /= 10, k++);
+        for (int i = 0; i < k / 2; i++) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        if (k % 2) {
+            x /= 10;
+        } 
+        return x == y;
     }
 };
